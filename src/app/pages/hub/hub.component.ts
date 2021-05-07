1d3c96ed4f0ee157f5b7f1088e1062b9cbf6e42e
Arthur <arthur.bosshardt@gmail.com>
2021-05-07 10:47:48 +0200

    Correction on user page and change budgy icon

--- src/app/pages/hub/hub.component.ts
+++ src/app/pages/hub/hub.component.ts
@@ -13,22 +13,19 @@ export class HubComponent implements OnInit {
   ngOnInit(): void {
     (function ($) {
       $(document).ready(function(){
-        let text = document.getElementById('text')
         let btn = document.getElementById('btn')
         let header = document.getElementById('header')
         let clouds = document.getElementById('clouds')
         let sun = document.getElementById('sun')
         let hills = document.getElementById('hills')
-        let forest = document.getElementById('forest')
-        let front = document.getElementById('front')
 
         window.addEventListener('scroll', function() {
           let value = window.scrollY;
           clouds.style.top = value * -1.5 + 'px';
           sun.style.top = value * -1.5 + 'px';
-          btn.style.marginTop = value * 1.5 + 'px';
-          header.style.top = value * 0.5 + 'px';
-          forest.style.top = value * 0.5 + 'px';
+          btn.style.marginTop = value * 0.7 + 'px';
+          hills.style.top = value * -0.2 + 'px';
+          header.style.top = value * -0.2 + 'px';
         })
       });
     })(jQuery);
