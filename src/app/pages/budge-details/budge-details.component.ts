8041440959cd83dcaf5849b9b9c7ebea8580d3d3
Arthur <arthur.bosshardt@gmail.com>
2021-05-15 19:14:59 +0200

    Change to do list logic 2/2

--- src/app/pages/budge-details/budge-details.component.ts
+++ src/app/pages/budge-details/budge-details.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute } from '@angular/router';
 import companies from '../../../assets/config/companies-info.json';
 import { BudgeInfoClassic } from '../../models/budge-info';
 import { NumberFormatStyle } from '@angular/common';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-budge-details',
@@ -19,6 +20,7 @@ export class BudgeDetailsComponent implements OnInit {
   infoUser: Array<BudgeInfoClassic>;
   public companies:{company_name:string, types: string[], url_website:string, url_logo:string}[] = companies;
   budgeInfos = new BudgeInfoClassic("", "", 0, 0, new Array<string>(), new Array<string>());
+  taskToAdd: string;
 
   constructor(private _router: ActivatedRoute) { }
 
@@ -32,9 +34,9 @@ export class BudgeDetailsComponent implements OnInit {
 
 
     this.companies = this.companies.filter(company => company.types.includes(this.type));
-
     this.infoUser = JSON.parse(localStorage.getItem('budgeInfo'));
     this.budgeInfos = this.infoUser.filter(x => x.type == this.type)[0];
+    console.log(this.budgeInfos.tasksToDo.length);
   }
 
   ngOnDestroy() {
@@ -42,7 +44,7 @@ export class BudgeDetailsComponent implements OnInit {
   }
 
   saveBudgeInfos() {
-    this.infoUser = this.infoUser.filter(x => x.type != this.type)
+    this.infoUser = this.infoUser.filter(x => x.type != this.type);
     var infoUserToSend = {
       type: this.budgeInfos.type,
       company_name: this.budgeInfos.company_name,
@@ -54,5 +56,21 @@ export class BudgeDetailsComponent implements OnInit {
     console.log(infoUserToSend);
     this.infoUser.push(infoUserToSend);
     localStorage.setItem('budgeInfo', JSON.stringify(this.infoUser));
+  }
+
+  addToDo(task: string) {
+    this.budgeInfos.tasksToDo.push(task);
+    this.saveBudgeInfos();
+  }
+
+  addToDone(task: string) {
+    this.budgeInfos.tasksDone.push(task);
+    this.budgeInfos.tasksToDo = this.budgeInfos.tasksToDo.filter(taskToDo => taskToDo != task);
+    this.saveBudgeInfos();
+  }
+
+  removeTask(task: string) {
+    this.budgeInfos.tasksDone = this.budgeInfos.tasksDone.filter(taskDone => taskDone != task);
+    this.saveBudgeInfos();
   }
 }
